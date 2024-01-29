65c57c81149a148782d81dc48b5b44701d18fda5
hellosonic-r <hellommmh@gmail.com>
2024-01-29 23:11:29 +0900

    feat : 기본 페이지 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,6 @@
 export { default as HomePage } from "./HomePage";
 export { default as SignInPage } from "./SignInPage";
 export { default as SignUpPage } from "./SignUpPage";
+export { default as SplashPage } from "./SplashPage";
+export { default as ProfilePage } from "./ProfilePage";
+export { default as TestPage } from "./TestPage";
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,21 @@
-import { HomePage, SignInPage, SignUpPage } from "@/pages";
+import {
+  HomePage,
+  ProfilePage,
+  SignInPage,
+  SignUpPage,
+  SplashPage,
+} from "@/pages";
 import { Route, Routes } from "react-router-dom";
 
 const RouterComponent = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<SplashPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </>
   );
